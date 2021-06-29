ff98d90805f15c27b915828c44cc5a4d9eb38c81
raouf nasr <raoufnasr59@gmail.com>
2021-06-29 18:11:25 +0100

    fix get all product

--- workspace/backadmin/src/app/_services/product.service.ts
+++ workspace/backadmin/src/app/_services/product.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  public config = {
+    apiUrl: environment.url
+  };
+  constructor(private http: HttpClient) { }
+
+
+  gtAllProduct() {
+    return this.http.get<any>(`${this.config.apiUrl}product/getAll`);
+  }
 }
